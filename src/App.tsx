816929e4ec1072fd24fe73b6c78a0008945a346a
RodrigoDGoulart <rodinizgoulart@gmail.com>
2025-05-14 23:40:08 -0300

    feat: gallery component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Header from "./components/Header";
+import Gallery from "./components/Gallery";
+const url = 'https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 function App() {
   return (
     <div>
-      
+      <Gallery images={[url, url, url, url, url, url, url,]} />
     </div>
   );
 }
---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,49 @@
+import classNames from "classnames";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useState } from "react";
+
+interface Props {
+  images: string[];
+}
+
+export default function Gallery({images}: Props) {
+  const [selectedImgIndex, setSelectedImgIndex] = useState<number>(0);
+
+  return(
+    <div className="flex flex-col gap-[12px] w-[300px]">
+      <div className="relative w-[300px] h-[300px]">
+        <img src={images[selectedImgIndex]} className="object-cover object-center w-full h-full" />
+        <button 
+          className="absolute top-1/2 left-[12px] bg-black/20 rounded-full cursor-pointer" 
+          onClick={() => setSelectedImgIndex(prev => prev-1)} 
+          disabled={selectedImgIndex === 0}
+        >
+          <ChevronLeft size={36} color={selectedImgIndex === 0 ? "gray" : "white"} />
+        </button>
+        <button 
+          className="absolute top-1/2 right-[12px] bg-black/20 rounded-full cursor-pointer" 
+          onClick={() => setSelectedImgIndex(prev => prev+1)} 
+          disabled={selectedImgIndex === images.slice(0, 6).length - 1}
+        >
+          <ChevronRight size={36} color={selectedImgIndex === images.slice(0, 6).length - 1 ? "gray" : "white"} />
+        </button>
+      </div>
+      <div className="flex gap-[6px] overflow-hidden">
+        {images.slice(0, 6).map((img, index) => (
+          <button 
+            key={index} 
+            className={classNames(
+              "w-[48px] h-[48px]",
+              { "opacity-50": index !== selectedImgIndex },
+            )}
+            onClick={() => setSelectedImgIndex(index)}
+          >
+            <img 
+              src={img} 
+              className="w-full h-full object-cover object-center" />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
